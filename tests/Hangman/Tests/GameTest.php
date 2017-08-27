00ce5aaf58afacf3fd0a4758ee1213ba8fdf8cf5
amine.hafs@gmail.com <iinkutchivers@live.fr>
2017-08-27 11:25:58 +0200

    test unitaire pour verifier si la lettre que l'utilisateur a saisie a ete trouver ou pas

--- tests/Hangman/Tests/GameTest.php
+++ tests/Hangman/Tests/GameTest.php
@@ -5,6 +5,21 @@ namespace Hangman\Tests;
 use Hangman\Game;
 use Hangman\Word;
 
-class GameTest extends \PHPUnit_Framework_TestCase
+class GameTest extends \PHPUnit\Framework\TestCase
 {
+    public function testisLetterFound()
+    {
+        $game = new Game(new Word('php'));
+
+
+        // result prend la lettre que l'utilisateur a choisi
+        $game->tryLetter('p');
+
+        // permet de verifier si la letttre a ete trouver dans le  mot
+        $this->assertTrue($game->isLetterFound('p'));
+
+        // permet de verifier si la letttre n'a pas ete trouver dans le  mot
+        // la lettre n'a pas ete trouver pcq l'utilisateur a saisie p
+        $this->assertFalse($game->isLetterFound('h'));
+    }
 }
